b9d79f8958d4c56affaccc0f38adccf1e7bb5e50
Dawit Fikadu <dawitf645@gmail.com>
2026-07-17 00:07:06 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,1 +1,9 @@
-.
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [{ protocol: "https", hostname: "res.cloudinary.com" }],
+  },
+};
+
+export default nextConfig;
