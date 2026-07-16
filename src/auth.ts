175b6c8ffa8da0c3782ea3fe215f77916a0c8abc
Dawit Fikadu <dawitf645@gmail.com>
2026-07-17 01:21:20 +0300

    Update auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -1,1 +1,25 @@
-.
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { db } from "@/lib/db";
+import bcrypt from "bcryptjs";
+import authConfig from "./auth.config";
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(db),
+  session: { strategy: "jwt" },
+  ...authConfig,
+  callbacks: {
+    async session({ session, token }) {
+      if (session.user && token.sub) {
+        session.user.id = token.sub;
+      }
+      return session;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token.sub = user.id;
+      }
+      return token;
+    }
+  },
+});
