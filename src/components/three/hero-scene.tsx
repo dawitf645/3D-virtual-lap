e5e08185d344ae2bb622cb2c015a4a417bbb7665
Dawit Fikadu <dawitf645@gmail.com>
2026-07-16 23:27:07 +0300

    Update hero-scene.tsx

--- src/components/three/hero-scene.tsx
+++ src/components/three/hero-scene.tsx
@@ -1,1 +1,32 @@
-.
+"use client";
+
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Float, Environment } from "@react-three/drei";
+import { Physics, RigidBody } from "@react-three/rapier";
+
+function Orb() {
+  return (
+    <Float speed={2} rotationIntensity={1} floatIntensity={1}>
+      <mesh>
+        <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial color="#3b82f6" metalness={0.6} roughness={0.2} />
+      </mesh>
+    </Float>
+  );
+}
+
+export default function HeroScene() {
+  return (
+    <div className="h-[420px] w-full rounded-2xl border border-white/10 bg-black/30">
+      <Canvas camera={{ position: [0, 0, 4], fov: 50 }}>
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[3, 3Body type="fixed">
+            <Orb />
+          </RigidBody>
+        </Physics>
+        <Environment preset="city" />
+        <OrbitControls enablePan={false} />
+      </Canvas>
+    </div>
+  );
+}
