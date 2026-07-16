7023d18f9ad2105696b5b738b45c1c71010afe1c
Dawit Fikadu <dawitf645@gmail.com>
2026-07-17 01:19:15 +0300

    Update auth.config.ts

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,1 +1,22 @@
-.
+import Google from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
+import type { NextAuthConfig } from "next-auth";
+
+export default {
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    Credentials({
+      name: "Credentials",
+      credentials: {
+        email: {},
+        password: {},
+      },
+      async authorize(credentials) {
+        return null;
+      },
+    }),
+  ],
+} satisfies NextAuthConfig;
